03a45b2ced8e72852848ed5406a61414e4eb1470
Garrett Battaglia <garrett@garrettbattaglia.info>
2017-12-10 19:10:37 -0600

    check for lockfile when starting

--- src/main.c
+++ src/main.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
+#include <unistd.h>
 #include "commandLine/CommandlineParser.h"
+
+char lockfile_exists(){
+	return !access("/etc/coldbrew/lockfile", F_OK);
+}
 
 int main(int argc, char **argv)
 {
+	if (lockfile_exists()){
+		fprintf(stderr, "Lockfile exists, exiting... \n if you believe this is an error verify coldbrew isn't running then delete \"/etc/coldbrew/lockfile\"");
+		exit(-1);
+	}
 	parseCommandLine(argc-1, argv +1);
+
 }
