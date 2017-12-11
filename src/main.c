bae16f70304068171c9da20da578acf3ee3863d9
Garrett Battaglia <garrett@garrettbattaglia.info>
2017-12-10 19:30:04 -0600

    set lockfile path to work w/o root if DEV is defed also defined DEV in cmake file

--- src/main.c
+++ src/main.c
@@ -2,16 +2,33 @@
 #include <unistd.h>
 #include "commandLine/CommandlineParser.h"
 
+#ifdef DEV
+#define LOCKFILE "./lockfile"
+#else
+#define LOCKFILE "/etc/coldbrew/lockfile"
+#endif
+
 char lockfile_exists(){
-	return !access("/etc/coldbrew/lockfile", F_OK);
+	return !access(LOCKFILE, F_OK);
+}
+
+void lock(){
+	system("touch "  LOCKFILE);
+}
+
+void unlock(){
+	system("rm " LOCKFILE);
 }
 
 int main(int argc, char **argv)
 {
 	if (lockfile_exists()){
-		fprintf(stderr, "Lockfile exists, exiting... \n if you believe this is an error verify coldbrew isn't running then delete \"/etc/coldbrew/lockfile\"");
+		fprintf(stderr, "Lockfile exists, exiting... \n if you believe this is an error verify coldbrew isn't running then delete \""LOCKFILE" \" ");
 		exit(-1);
 	}
+	lock();
 	parseCommandLine(argc-1, argv +1);
+	//DO stuff here
+	unlock();
 
 }
