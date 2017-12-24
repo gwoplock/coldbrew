eaae78acbb56d3557f183910d0ed9d52b0dd2bbd
Garrett Battaglia <garrett@garrettbattaglia.info>
2017-12-24 12:01:41 -0600

    read int from file also fixed a magic number

--- src/Commands/Install.c
+++ src/Commands/Install.c
@@ -10,7 +10,7 @@
 #include "Install.h"
 #include "../utils/print.h"
 
-const short SHABANG = 0x2123;
+const short SHABANG = 0x2321;
 
 const short GZ_MAGIC_NUM = 0;
 
@@ -26,6 +26,7 @@ enum type get_install_type(struct target targ)
 	FILE *file;
 	if (config.mode_opts != NULL && ((struct install_options *) (config.mode_opts))->local == 1) {
 		file = fopen(targ.name, "r");
+
 	} else {
 		char tmp_dir_template[256];
 		strcat(tmp_dir_template, "/tmp/coldbrew.");
@@ -37,14 +38,17 @@ enum type get_install_type(struct target targ)
 		file = fopen(pkg_loc, "r");
 	}
 
-	if (file == NULL){
-		dbfprintf(VERBOSE, stderr, "fopen had an error and returned an errorno of %i, translated it is \"%s\"\n", errno, strerror(errno));
+	if (file == NULL) {
+		dbfprintf(VERBOSE, stderr,
+		          "fopen had an error and returned an errorno of %i, translated it is \"%s\"\n", errno,
+		          strerror(errno));
 		dbfprintf(NORMAL, stderr, "there was an error opening the package");
 		unlock();
 		exit(4);
 	}
 	enum type ret;
 	int file_head = get_first_int(file);
+	dbprintf(DEBUG, "first 4 bytes of the package are %x\n", file_head);
 	if (file_head == GZ_MAGIC_NUM) {
 		ret = PACKAGE;
 	} else if (file_head >> 16 == SHABANG) {
@@ -61,4 +65,11 @@ char *download_file(struct target targ, char *tmp_dir)
 
 int get_first_int(FILE *file)
 {
+	int *ret = calloc(1, sizeof(int));
+	for (int i = 0; i < 3; i++) {
+		fread(ret, 1, 1, file);
+		*ret = *ret << 8;
+	}
+	fread(ret, 1, 1, file);
+	return *ret;
 }
