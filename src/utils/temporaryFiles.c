cdabe585d6e7f8a52ac4b90c1cc6c40a321c8c12
Garrett Battaglia <garrett@garrettbattaglia.info>
2017-12-28 15:48:54 -0500

    added include for linux for mkdtemp

--- src/utils/temporaryFiles.c
+++ src/utils/temporaryFiles.c
@@ -3,6 +3,8 @@
 //
 
 #include <string.h>
+#include <stdlib.h>
+#include <errno.h>
 #include <unistd.h>
 #include "temporaryFiles.h"
 #include "print.h"
@@ -14,7 +16,12 @@ char *create_tmp_dir(char *dir_name)
 	strcat(tmp_dir_template, "/tmp/coldbrew.");
 	strcat(tmp_dir_template, dir_name);
 	strcat(tmp_dir_template, ".XXXXXX");
-	char* tmp = mkdtemp(tmp_dir_template);
+	char *tmp = mkdtemp(tmp_dir_template);
+	if (tmp == NULL) {
+		dbfprintf(VERBOSE, stderr,
+		          "mkdtemp had an error and returned an errorno of %i, translated it is \"%s\"\n", errno,
+		          strerror(errno));
+	}
 	dbprintf(DEBUG, "target: %s, template: %s, tempdir: %s\n", dir_name, tmp_dir_template, tmp);
 	return tmp;
 }
