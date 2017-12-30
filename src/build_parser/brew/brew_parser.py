8bffa0f2ca623fda4141848970c86a13993ad41f
xxc3nsoredxx <xxc3ncoredxx@gmail.com>
2017-12-30 00:07:25 -0600

    Hooray, extracts some information
    Not all yet

--- src/build_parser/brew/brew_parser.py
+++ src/build_parser/brew/brew_parser.py
@@ -18,8 +18,15 @@ with open(argv[1], "r") as infile:
   sha256 = "NULL"
   instructions = {}
 
+  # read lines into an array
   lines = infile.readlines()
   cx = 0
+
+  #remove the initial class... and final end
+  del lines[0]
+  del lines[-1]
+
+  # loop through the rest of the lines removing useless ones
   length = len(lines)
   while cx < length:
     line = lines[cx].strip()
@@ -30,8 +37,28 @@ with open(argv[1], "r") as infile:
       lines[cx] = line
       cx = cx + 1
 
+  # parse the first bit
+  temp = lines[0].split("\"")[1]
+  if temp is not "":
+    desc = temp
+  del lines[0]
+  temp = lines[0].split("\"")[1]
+  if temp is not "":
+    homepage = temp
+  del lines[0]
+  temp = lines[0].split("\"")[1]
+  if temp is not "":
+    url = temp
+  del lines[0]
+  temp = lines[0].split("\"")[1]
+  if temp is not "":
+    sha256 = temp
+  del lines[0]
+
   for line in lines:
     print(line)
+
+  print()
 
   print("Desc:", desc)
   print("Homepage:", homepage)
