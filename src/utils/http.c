bb0d3f792726c9fa47570adece5771989fd6da9d
Garrett Battaglia <garrett@garrettbattaglia.info>
2017-12-30 15:15:11 -0500

    create file to download intocreate file to download intocreate file to download intocreate file to download intocreate file to download intocreate file to download intocreate file to download intocreate file to download intocreate file to download into

--- src/utils/http.c
+++ src/utils/http.c
@@ -8,6 +8,7 @@
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <fcntl.h>
 #include "http.h"
 
 
@@ -18,6 +19,7 @@ void download(struct resource to_dl, char *local_loc, char *local_filename)
 	if (socket_num == -1) {
 		//error
 	}
+
 	//connect to server
 	struct sockaddr_in serv;
 	serv.sin_addr.s_addr = inet_addr(to_dl.IP);
@@ -27,17 +29,28 @@ void download(struct resource to_dl, char *local_loc, char *local_filename)
 	if (res < 0) {
 		//error
 	}
+
 	//send get request
 	char *get_start = "GET ";
 	char *get_tail = " HTTP/1.1\r\n\r\n";
 	size_t get_req_leng = strlen(get_start) + strlen(to_dl.location) + strlen(get_tail);
 	char *get_req = calloc(sizeof(char), get_req_leng);
+	strcat(get_req, get_start);
+	strcat(get_req, to_dl.location);
+	strcat(get_req, get_tail);
 	res = send(socket_num, get_req, get_req_leng, 0);
 	if (res < 0) {
 		//error
 	}
+
+	//open file for response
+	size_t file_name_leng = strlen(local_loc) + strlen(local_filename);
+	char *file_name = calloc(file_name_leng, sizeof(char));
+	strcat(file_name, local_loc);
+	strcat(file_name, local_filename);
+	int file_num = creat(file_name, 0777);
+
 	//receive response
-	//TODO open file
 	char reply[256];
 	do {
 		res = read(socket_num, reply, 256);
