77ee7699a545e8d984d1427a616d641a0d055478
Garrett Battaglia <garrett@garrettbattaglia.info>
2018-01-03 09:50:01 -0500

    debug prints

--- src/utils/fileIO.c
+++ src/utils/fileIO.c
@@ -3,11 +3,15 @@
 //
 
 #include "fileIO.h"
+#include "print.h"
 
-int read_line(char* buffer, int buf_size, FILE* file){
-	char last_read ='\0';
-	for(int i = 0; i< buf_size && last_read != '\n'; i++){
+int read_line(char *buffer, int buf_size, FILE *file)
+{
+	char last_read = '\0';
+	for (int i = 0; i < buf_size && last_read != '\n'; i++) {
 		buffer[i] = last_read;
 		last_read = getc(file);
+		dbprintf(DEBUG, "just read: %c \n", last_read);
 	}
+	dbprintf(DEBUG, "exited loop\n");
 }
