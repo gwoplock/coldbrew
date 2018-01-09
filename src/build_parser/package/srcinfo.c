35f3b63e524ea3dd5a93fd4713170e42837dce7a
Garrett Battaglia <garrett@garrettbattaglia.info>
2018-01-09 17:46:42 -0600

    switch off type

--- src/build_parser/package/srcinfo.c
+++ src/build_parser/package/srcinfo.c
@@ -8,6 +8,7 @@
 #include "srcinfo.h"
 #include "../../utils/fileIO.h"
 #include "../../utils/print.h"
+#include "../../utils/stringManip.h"
 
 void parse_srcinfo(FILE *pkginfo_file, struct target *targ)
 {
@@ -27,7 +28,7 @@ void parse_srcinfo(FILE *pkginfo_file, struct target *targ)
 		          errno,
 		          strerror(errno));
 		unlock();
-		exit(6);
+		exit(7);
 	} else {
 		//TODO
 		//didnt read full line
@@ -39,6 +40,34 @@ void proc_srcinfo_line(char *buffer, int buf_size, struct target *targ)
 {
 	dbprintf(VERBOSE, "parsing line: %s\n", buffer);
 	//split into path, type, type fields
+	char **line = calloc(3, sizeof(char *));
+	char **spl = string_split(buffer, ' ');
+	line[0] = spl[0];
+	char *temp = spl[1];
+	spl = string_split(spl[1], ' ');
+	if (spl == NULL) {
+		line[1] = temp;
+	} else {
+		line[1] = spl[0];
+	}
+	dbprintf(DEBUG, "path: %s, type: %s\n", line[0], line[1]);
+
 	//switch on type
+	if (strcmp(line[1], "dir") == 0) {
+
+	} else if (strcmp(line[1], "file") == 0) {
+		spl = string_split(spl[1], ' ');
+		line[2] = spl[0];
+		dbprintf(DEBUG, "type args: %s", line[2]);
+	} else if (strcmp(line[1], "link") == 0) {
+		spl = string_split(spl[1], ' ');
+		line[2] = spl[0];
+		dbprintf(DEBUG, "type args: %s", line[2]);
+	} else {
+		dbfprintf(NORMAL, stderr, "the .SRCINFO was invalid.\n");
+		dbfprintf(VERBOSE, stderr, "The type \"%s\" was invalid, ", line[1]);
+		unlock();
+		exit(7);
+	}
 	//apply action
 }
